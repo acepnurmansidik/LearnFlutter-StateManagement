a79b1262393eccdb428bccb881e8e9bda91a22b3
acepnurmansidik <acepnurmansidik@gmail.com>
2022-04-14 14:57:14 +0700

    add state

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:state_management/theme.dart';
 import 'package:state_management/widgets/header.dart';
-import 'package:state_management/widgets/optionsMenu.dart';
 
 void main() => runApp(const PaymentApp());
 
@@ -13,24 +12,38 @@ class PaymentApp extends StatefulWidget {
 }
 
 class _PaymentAppState extends State<PaymentApp> {
-
+  int selectedIndex = -1;
   @override
   Widget build(BuildContext context) {
+    Widget options(int index) {
+      return GestureDetector(
+        onTap: (() {
+          setState(() {
+            selectedIndex = index;
+          });
+        }),
+        child: Container(
+          margin: EdgeInsets.all(20),
+          padding: EdgeInsets.all(20),
+          decoration: BoxDecoration(
+              border: Border.all(
+                  color: selectedIndex == index
+                      ? Color(0xFF007DFF)
+                      : Color(0xFF4D5B7C)),
+              borderRadius: BorderRadius.all(Radius.circular(14))),
+        ),
+      );
+    }
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Color(0xFF04112F),
-        body: SafeArea(
-          child: ListView(
-            children: [
-              HeaderWidget(),
-              optionsMenu(),
-              optionsMenu(),
-              optionsMenu()
-            ],
-          ),
-        )
-      ),
+          backgroundColor: Color(0xFF04112F),
+          body: SafeArea(
+            child: ListView(
+              children: [HeaderWidget(), options(0), options(1), options(2)],
+            ),
+          )),
     );
   }
 }
